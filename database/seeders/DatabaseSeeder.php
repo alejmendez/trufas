d090b54b4af693e63648df465d0febccb86b4909
Alejandro Méndez <alejmendez.87@gmail.com>
2024-01-11 11:06:45 -0300

    prevent seeder execution

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        if (User::count() > 0) {
+            return;
+        }
+
         $this->call([
             RolesSeeder::class,
             FieldsSeeder::class,
