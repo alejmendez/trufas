c6cdc1437bc4eda42cf6a78134ba575fc29be74b
Alejandro Méndez <alejmendez.87@gmail.com>
2024-01-12 02:33:51 -0300

    add user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,7 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use App\Models\User;
+
 class UserSeeder extends Seeder
 {
     /**
@@ -15,14 +17,52 @@ class UserSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        User::create([
             'name' => 'Alejandro',
             'email' => 'alejmendez.87@gmail.com',
             'password' => Hash::make('1234qwer'),
             'dni' => '26.604.667-7',
-            'avatar' => '',
+            'avatar' => null,
             'last_name' => 'Méndez',
             'phone' => '+56 9 68005128',
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::create([
+            'name' => 'Fernando',
+            'email' => 'fud@undo.cl',
+            'password' => Hash::make('1234qwer'),
+            'dni' => '30.052.194-0',
+            'avatar' => null,
+            'last_name' => 'Undurraga',
+            'phone' => null,
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::create([
+            'name' => 'Cristian',
+            'email' => 'cristiancorreac@gmail.com',
+            'password' => Hash::make('1234qwer'),
+            'dni' => '30.335.289-9',
+            'avatar' => null,
+            'last_name' => 'Correa',
+            'phone' => null,
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::create([
+            'name' => 'Maria',
+            'email' => 'camporamaria@gmail.com',
+            'password' => Hash::make('1234qwer'),
+            'dni' => '36.399.118-1',
+            'avatar' => null,
+            'last_name' => 'Campora',
+            'phone' => null,
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
         ]);
 
         User::find(1)->assignRole('Super Admin');
